bacd36a1501cd3ae868a977497f901f68cab3072
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-25 20:23:06 -0800

    added directory for rewrite

--- main2.js
+++ main2.js
@@ -2,103 +2,48 @@
  * Created by Patrick on 12/6/2016.
  */
 
-$(document).ready(function () {
-    $(".number").click(numHandler);
-    $(".operator").click(opsHandler);
-    $(".equal").click(processInput);
-    $(".clear").click(clearHandler);
+/*document.addEventListener("DOMContentLoaded", function() {
 
+});*/
+
+
+
+
+const calculator = document.querySelector('.calculator');
+console.log(calculator);
+const keys = calculator.querySelector('.calculator__keys');
+console.log(keys);
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const action = key.dataset.action;
+        console.log(action);
+        if (!action) {
+            console.log('number was pushed');
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            console.log('operator selected');
+        }
+        if (action === 'decimal') {
+            console.log('decimal selected');
+        }
+        if (action === 'clear') {
+            console.log('clear selected');
+        }
+        if (action === 'clearall') {
+            console.log('clearall selected');
+        }
+        if (action === 'equals') {
+            console.log('equals selected');
+        }
+    }
 });
 
-var operationArray = [""];
-var operationIndex = 0;
-var operator;
-var somebutton;
 
-//handles button input
-
-function numHandler() {
-    somebutton = {
-        type: "number",
-        value: $(this).text()
-    };
-    console.log('Somebutton ' + somebutton);
-    operationArray[operationIndex] += $(this).text();
-    console.log('Array ' + operationArray);
-    displayData();
-}
-
-//handles math with more than two numbers
-
-function opsHandler() {
-    if (operationArray.length === 3); {
-        processInput();
-    }
-    somebutton = {
-        type: "operator",
-        value: $(this).text()
-    }
-    if (operationArray[0] === "") {
-        return;
-    } else {
-        console.log('Op array ' + somebutton);
-        operationArray[1] = $(this).text();
-        //operationIndex = operationIndex + 2;
-        //operationArray[operationIndex] = "";
-        console.log('Op array ' + operationArray);
-        displayData();
-    }
-}
-
-//clear button to clear array
-
-function clearHandler() {
- somebutton = {
- type: "clear",
- value: $(this).text()
- };
- operationArray[""] = $(this).text();
- };
-
-//handles numbers with more than one digit
-
-function processInput(){
-    x = operationArray.length-1;
-    if (operationArray[x] === undefined || operationArray[x] typeof =) {
-        return;
-    } else (operationArray.length >= 3); {
-                var num1 = parseFloat(operationArray[0]);
-                var num2 = parseFloat(operationArray[2]);
-                var operator = operationArray[1];
-                var result = doMath(num1, num2, operator);
-                operationArray.splice(0, 3, result);
-                operationIndex = 0;
-            }
-            displayData();
-        }
-
-//displays numbers on calculator
-
-function displayData(){
-    var output = operationArray.join('');
-
-    $(".display > .one").text(output);
-}
-
-//performs arithmetic
-
-function doMath(num1, num2, operator) {
-    console.log('do_math operator is ' + operator);
-    if (operator === "+") {
-        return(num1 + num2);
-    } else if (operator === "-") {
-        return(num1 - num2);
-    } else if (operator === "x") {
-        return(num1 * num2);
-    } else if (num2 === 0) {
-        return ("error");
-    } else {
-        return(num1 / num2);
-    }
-}
 
