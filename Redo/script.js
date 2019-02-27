04da48905663a6e1f5ef975fd0cc4c6a7dabdeda
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-26 16:23:51 -0800

    added event listener calling function to detect which button pushed

--- Redo/script.js
+++ Redo/script.js
@@ -1,1 +1,20 @@
-document.addEventListener
+const math = [];
+
+function detectButton(event) {
+    const button = event.path[0].value;
+    const buttonClass = event.path[0].className;
+    if (buttonClass === 'number') {
+        //parseFloat(button);
+        math.push(button);
+        console.log(math);
+        console.log(typeof math[0]);
+    } else {
+        math.push(button);
+        console.log(math);
+    }
+
+}
+
+document.addEventListener("click", detectButton);
+    console.log(event);
+    //console.log(typeof parseFloat(button));
