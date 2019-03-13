e6e5f9f8cea15a57cd99883bfe166e4990320fe2
Patrick McMahon <pmcmahon14@gmail.com>
2019-03-12 18:55:07 -0700

    button value shows up on display'

--- Redo/script.js
+++ Redo/script.js
@@ -2,10 +2,13 @@ const math = [];
 
 function detectButton(event) {
     const button = event.path[0].value;
+    console.log(button);
+    console.log(typeof button);
     const buttonClass = event.path[0].className;
     if (buttonClass === 'number') {
         //parseFloat(button);
         math.push(button);
+        document.querySelector('#display').innerHTML = math.toString();
         console.log(math);
         console.log(typeof math[0]);
     } else {
