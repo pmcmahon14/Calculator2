9bdd8fd742004ef91ebf4d49b7c312df3062d2ad
Patrick McMahon <pmcmahon14@gmail.com>
2019-03-13 22:29:01 -0700

    basic calculations work, need to fix bugs with decimal points

--- Redo/script.js
+++ Redo/script.js
@@ -1,18 +1,13 @@
 let displayVal = '';
 let prevButtonType = null;
-let decimalTest = false;
 
 var checkButtonType = (button, buttonClass) => {
     switch (buttonClass) {
         case 'number':
-            console.log(button, buttonClass);
             prevButtonType = buttonClass;
-            console.log(prevButtonType);
             updateDisplay(button, buttonClass);
             break;
         case 'operator':
-            console.log(prevButtonType);
-            console.log(prevButtonType === 'operator');
             if (prevButtonType === 'operator') {
                 return;
             } else {
@@ -21,9 +16,7 @@ var checkButtonType = (button, buttonClass) => {
             }
             break;
         case 'decimal':
-            console.log(typeof displayVal);
             const array = displayVal.split('');
-            console.log(array);
             if (array.includes('.')) {
                 return;
             } else {
@@ -32,6 +25,7 @@ var checkButtonType = (button, buttonClass) => {
             }
             break;
         case 'equal':
+            displayVal = (document.querySelector('#display').innerHTML = eval(displayVal));
 
     }
 };
@@ -44,32 +38,9 @@ var updateDisplay = (button, buttonClass) => {
 };
 
 function detectButton(event) {
-    const button = event.target.innerText;
+    const button = event.target.value;
     const buttonClass = event.target.className;
     checkButtonType(button, buttonClass);
-    /*switch (buttonClass) {
-        case 'number':
-            console.log(buttonClass !== 'number');
-
-            document.querySelector('#display').innerHTML = displayVal;
-            displayVal += button;
-            break;
-        case 'operator':
-            console.log(displayVal[displayVal.length-1].buttonClass);
-            if (displayVal.length-1 !== (buttonClass === 'operator')) {
-                displayVal += button;
-
-            }
-    }
-
-    if (buttonClass === 'number') {
-
-        console.log(displayVal);
-    } else {
-        //math.push(button);
-        console.log(displayVal);
-    }*/
-
 }
 
 document.addEventListener("click", detectButton);
